d0ba1b659a66cbe87790a4cada2ae7a81da388d5
Eduardo Fonnegra <eduardo@pitchnode.com>
2026-02-20 02:43:27 +0000

    File changes

--- src/pages/UpdateArchive.jsx
+++ src/pages/UpdateArchive.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { createPageUrl } from "../utils";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
-import { Calendar, TrendingUp, DollarSign, Flame, Clock } from "lucide-react";
+import { Plus } from "lucide-react";
 import UpdatePreview from "../components/update/UpdatePreview";
 
 export default function UpdateArchive() {
